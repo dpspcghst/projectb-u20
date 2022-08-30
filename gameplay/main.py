5cb44e4abfe86812ff5cc0c0c674294a44a5728f
deepspaceghost <deepspaceghost@protonmail.com>
2022-08-30 12:12:57 -0400

    Create main.py

---
+++ gameplay/main.py
@@ -0,0 +1,21 @@
+# gameplay
+
+class Game():
+  """
+  """
+  
+  def __init__(self):
+    """
+    """
+    
+    pass
+  
+  def run(self):
+    """
+    """
+    
+    pass
+
+if __name__ == "__main__":
+  game = Game()
+  game.run()
