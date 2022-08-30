2567c1fe7b279ebaccb15946c82a477d5f1c8b12
deepspaceghost <deepspaceghost@protonmail.com>
2022-08-30 12:15:26 -0400

    Create main.py

---
+++ perspective/main.py
@@ -0,0 +1,21 @@
+# perspective
+
+class Game():
+	"""
+	"""
+	
+	def __init__(self):
+		"""
+		"""
+		
+		pass
+	
+	def run(self):
+		"""
+		"""
+		
+		pass
+
+if __name__ == "__main__":
+	game = Game()
+	game.run
