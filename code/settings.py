fa16b38d8897a567de1bf0b3c7fd6425e3066e88
deepspaceghost <deepspaceghost@protonmail.com>
2022-09-01 14:30:37 -0400

    Create settings.py

---
+++ code/settings.py
@@ -0,0 +1,17 @@
+LEVEL_MAP = [
+    "                            ",
+    "         XXXXXXXXXXXXXXXXXXX",
+    "                            ",
+    "XXXXXXXXXXXXXXXXXXXXXXX     ",
+    "                            ",
+    " XXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "                            ",
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+]
+
+FPS = 60 # frames per second
+DARK_GRAY = (64, 64, 64)
+TILE_SIZE = 64
+HEIGHT = len(LEVEL_MAP) * TILE_SIZE # round(WIDTH / 2.39) for later use
+VIVID_PURPLE = (255, 0, 255)
+WIDTH = 1280
