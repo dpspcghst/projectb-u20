6493231a32a10913625ea4f816c7bf66cff80294
deepspaceghost <deepspaceghost@protonmail.com>
2022-09-01 14:30:57 -0400

    Create tile.py

---
+++ code/tile.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import VIVID_PURPLE
+
+class Tile(pygame.sprite.Sprite):
+    """
+    """
+
+    def __init__(self, position, size):
+        """
+        """
+
+        super().__init__()
+
+        self.image = pygame.Surface((size, size))
+        self.image.fill(VIVID_PURPLE)
+        self.rect = self.image.get_rect(topleft=position)
+
+    def update(self, x_shift):
+        """
+        """
+
+        self.rect.x += x_shift
